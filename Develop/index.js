a6dceb37fa40e01e0f9853796f1abbfb2c73ad0e
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-09-29 11:49:57 +1000

    Fixed questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,56 +1,61 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const { type } = require("os");
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: "input",
     name: "title",
-    message: "Please enter Project Title",
-    verify: (nameInput) => {
-      if (nameInput) {
+    message: "Please enter the title of the project",
+    validate: (title) => {
+      if (title) {
         return true;
       } else {
-        console.log("Error! Please provide a project title!");
+        console.log("Error! Please enter a project title");
         return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "Please enter your GitHub username",
-    verify: (githubInput) => {
-      if (githubInput) {
-        return true;
-      } else {
-        console.log("Error! Please enter your GitHub username!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "repo",
-    message: "Please enter the name of your repo",
-    verify: (repoInput) => {
-      if (repoInput) {
-        return true;
-      } else {
-        console.log("Error! Please enter the name of your repo!");
       }
     },
   },
   {
     type: "input",
     name: "description",
-    message: "Provide a description of your application",
-    verify: (descInput) => {
-      if (descInput) {
+    message: "Please enter a description of your project",
+    validate: (title) => {
+      if (title) {
         return true;
       } else {
-        console.log("Error! Please enter a description!");
+        console.log("Error! Please enter a project title");
+        return false;
+      }
+    },
+  },
+  {
+    type: "checkbox",
+    name: "tableOfContens",
+    message: "Would you like to add a table of contents?",
+    choices: ["Yes", "No"],
+    validate: (title) => {
+      if (title) {
+        return true;
+      } else {
+        console.log("Error! Please enter a project title");
+        return false;
+      }
+    },
+  },
+
+  {
+    type: "input",
+    name: "installation",
+    message: "Please add any installation infomation",
+    validate: (title) => {
+      if (title) {
+        return true;
+      } else {
+        console.log("Error! Please enter a project title");
         return false;
       }
     },
@@ -58,133 +63,33 @@ const questions = [
   {
     type: "input",
     name: "usage",
-    message: "Please provide information for using your application",
-    verify: (usageInput) => {
-      if (usageInput) {
+    message: "Please add any usage infomation",
+    validate: (title) => {
+      if (title) {
         return true;
       } else {
-        console.log(
-          " Error! Please provide information for using your application!"
-        );
+        console.log("Error! Please enter a project title");
         return false;
       }
     },
   },
   {
     type: "checkbox",
-    name: "contents",
-    message:
-      "Any additional sections you would like to include in your README?",
+    name: "licensing",
+    message: "Please Choice from the list of license",
     choices: [
-      {
-        name: "deployed application",
-        checked: false,
-      },
-      {
-        name: "installation",
-        checked: false,
-      },
-      {
-        name: "screenshots",
-        checked: false,
-      },
-      {
-        name: "technologies ",
-        checked: true,
-      },
-
-      {
-        name: "contributing",
-        checked: false,
-      },
-      {
-        name: "questions",
-        checked: false,
-      },
-      {
-        name: "credits",
-        checked: false,
-      },
-      {
-        name: "license",
-        checked: false,
-      },
+      "Apache",
+      "MIT",
+      "Mozilla-Public",
+      "GNU-General-Public",
+      "Common-Development-and Distribution",
+      "None",
     ],
-  },
-  {
-    type: "input",
-    name: "link",
-    message: "Please provide a link to your deployed application.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Deployed Application") > -1) {
+    validate: (title) => {
+      if (title) {
         return true;
       } else {
-        return false;
-      }
-    },
-    verify: (linkInput) => {
-      if (linkInput) {
-        return true;
-      } else {
-        console.log("Please enter a link!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "installation",
-    message:
-      "Please list any required packages for installation of your application.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Installation") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    verify: (installInput) => {
-      if (installInput) {
-        return true;
-      } else {
-        console.log("Please enter installation instructions!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Please provide license information.",
-    choices: ["MIT", "GNU", "Apache 2.0", "ISC"],
-    default: 0,
-    when: ({ contents }) => {
-      if (contents.indexOf("License") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    verify: (licenseInput) => {
-      if (licenseInput) {
-        return true;
-      } else {
-        console.log("Please provide license information!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "checkbox",
-    name: "built with",
-    message:
-      "Please select the technologies that your application was built with.",
-    choices: ["HTML", "CSS", "SASS", "JavaScript", "Node.js", "Express.js"],
-    default: 0,
-    when: ({ contents }) => {
-      if (contents.indexOf("Built With") > -1) {
-        return true;
-      } else {
+        console.log("Error! Please enter a project title");
         return false;
       }
     },
@@ -192,73 +97,46 @@ const questions = [
   {
     type: "input",
     name: "contributing",
-    message: "Please enter your guidelines for contributing.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Contributing") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    verify: (contributingInput) => {
-      if (contributingInput) {
-        return true;
-      } else {
-        console.log("Please enter guidelines for contributing!");
-        return false;
-      }
-    },
+    message: "Please enter any contributing infomation",
   },
   {
     type: "input",
-    name: "tests",
-    message: "Please enter test information for your application.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Tests") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    verify: (testsInput) => {
-      if (testsInput) {
-        return true;
-      } else {
-        console.log(
-          "What packages are required to run tests for your application?"
-        );
-        return false;
-      }
-    },
+    name: "email",
+    message: "Please enter your email address",
   },
   {
     type: "input",
-    name: "questions",
-    message:
-      "Please provide an email address for others to reach you with questions.",
-    when: ({ contents }) => {
-      if (contents.indexOf("Questions") > -1) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    verify: (questionsInput) => {
-      if (questionsInput) {
-        return true;
-      } else {
-        console.log("Please provide an email address!");
-        return false;
-      }
-    },
+    name: "github",
+    message: "Please your github user name",
+  },
+  {
+    type: "input",
+    name: "repo",
+    message: "Please your github repo name",
+  },
+  {
+    type: "input",
+    name: "deploymentlink",
+    message: "Please add your Deployment link ",
   },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log("Success! Information transferred to the README!");
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+  inquirer.prompt(questions).then(function (userInput) {
+    console.log(userInput);
+    writeToFile("README.md", generateMarkdown(userInput));
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,30 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` # ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents (
+${data.tableOfContens}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## licensing
+${data.licensing}
+
+## contributing
+${data.contributing}
+${data.email}
+${data.github}
+${data.repo}
+${data.depoLink}
+${data.deploymentlink}
 `;
 }
 
